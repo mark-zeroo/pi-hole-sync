2f1c1c70d381afe7c9e8fe296b4cfcb56ef61681
markzero <github@markzero.net>
2026-09-05 18:11:31 -0400

    adds syncing groups, list groups, allow/block status, and enabled/disabled status

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Simple utility script for synchronizing block lists between two pi-hole servers.
 
 TO-DO:
-- Synchronize allow lists.
 - Add support for multiple secondary servers.
 - 2FA support
--- sync_pihole.py
+++ sync_pihole.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import urllib.request
 import urllib.error
 import urllib.parse
@@ -128,20 +129,34 @@ def make_request(url, method="GET", headers=None, data=None):
         
     encoded_data = json.dumps(data).encode("utf-8") if data is not None else None
     req = urllib.request.Request(url, data=encoded_data, headers=req_headers, method=method)
+
+    max_retries = 3
+    delay = 0
     
-    try:
-        with urllib.request.urlopen(req) as response:
-            raw_body = response.read().decode("utf-8").strip()
-            if not raw_body:
-                return {}
-            try:
-                return json.loads(raw_body)
-            except json.JSONDecodeError:
-                return {"raw_response": raw_body}
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error {e.code} on {method} {url}")
-        print(e.read().decode("utf-8"))
-        raise
+    for attempt in range(max_retries):
+        try:
+            with urllib.request.urlopen(req) as response:
+                raw_body = response.read().decode("utf-8").strip()
+                if not raw_body:
+                    return {}
+                try:
+                    return json.loads(raw_body)
+                except json.JSONDecodeError:
+                    return {"raw_response": raw_body}
+        except urllib.error.HTTPError as e:
+            error_body = e.read().decode("utf-8")
+            print(f"HTTP Error {e.code} on {method} {url}")
+            print(error_body)
+
+            if "database is locked" in error_body:
+                if attempt == max_retries - 1:
+                    print(f"Max retries reached. Database remains locked.")
+                    raise
+                
+                delay += 1.5
+                print(f"Database locked (attempt {attempt + 1}/{max_retries}). Retrying in {delay}s")
+                time.sleep(delay)
+   
 
 def get_session_sid(base_url, password):
     """Authenticates and returns a session token."""
@@ -164,44 +179,105 @@ def logout_session(base_url, sid):
 
 def get_adlists(base_url, sid):
     """Gets the dictionary of current adlists from Pi-hole."""
-    res = make_request(f"{base_url}/api/lists?type=block", "GET", headers={"sid": sid})
+    res = make_request(f"{base_url}/api/lists", "GET", headers={"sid": sid})
     return res.get("lists", res)
 
-def add_adlist(base_url, sid, address, comment, enabled=True):
+def add_adlist(base_url, sid, address, comment, enabled, listType):
     """Adds a new adlist to the server."""
     payload = {
         "address": address,
         "comment": comment if comment is not None else "",
         "enabled": enabled
     }
-    url = f"{base_url}/api/lists?type=block"
+    url = f"{base_url}/api/lists?type={listType}"
     return make_request(url, "POST", headers={"sid": sid}, data=payload)
 
-def delete_adlist(base_url, sid, address):
+def delete_adlist(base_url, sid, address, listType):
     """Deletes an adlist by its URL address."""
     encoded_address = urllib.parse.quote(address, safe='')
-    url = f"{base_url}/api/lists/{encoded_address}?type=block"
+    url = f"{base_url}/api/lists/{encoded_address}?type={listType}"
     return make_request(url, "DELETE", headers={"sid": sid})
 
-def enable_adlist(base_url, sid, address, comment):
+def enable_adlist(base_url, sid, address, comment, listType):
     """Enable a previously disabled adlist"""
     payload = {
         "comment": comment if comment is not None else "",
-        "enabled": True
+        "enabled": True,
+        "type": listType
     }
     
-    url = f"{base_url}/api/lists/{address}?type=block"
+    url = f"{base_url}/api/lists/{address}?type={listType}"
     return make_request(url, "PUT", headers={"sid": sid}, data=payload)
 
-def disable_adlist(base_url, sid, address, comment):
+def disable_adlist(base_url, sid, address, comment, listType):
     """Disable a previously enabled adlist"""
     payload = {
         "comment": comment if comment is not None else "",
         "enabled": False
     }
     
+    url = f"{base_url}/api/lists/{address}?type={listType}"
+    return make_request(url, "PUT", headers={"sid": sid}, data=payload)
+
+def allow_adlist(base_url, sid, address):
+    payload = {
+        "type": "allow"
+    }
+    
     url = f"{base_url}/api/lists/{address}?type=block"
     return make_request(url, "PUT", headers={"sid": sid}, data=payload)
+
+def block_adlist(base_url, sid, address):
+    payload = {
+        "type": "block"
+    }
+    
+    url = f"{base_url}/api/lists/{address}?type=allow"
+    return make_request(url, "PUT", headers={"sid": sid}, data=payload)
+
+def update_adlist_groups(base_url, sid, address, listType, listGroups, prim_groups, sec_groups):
+    payload = {
+        "groups": listGroups
+    }
+
+    url = f"{base_url}/api/lists/{address}?type={listType}"
+    return make_request(url, "PUT", headers={"sid": sid}, data=payload)
+
+def get_groups(base_url, sid):
+    url = f"{base_url}/api/groups"
+    try:
+        res = make_request(url, "GET", headers={"sid": sid})
+        return res.get("groups")
+    except Exception as e:
+        print(f"Could not fetch groups: {e}")
+        
+def create_group(base_url, sid, name, comment, enabled=True):
+    payload = {
+        "name": name,
+        "comment": comment if comment is not None else "",
+        "enabled": enabled
+    }
+
+    url = f"{base_url}/api/groups"
+    return make_request(url, "POST", headers={"sid": sid}, data=payload)
+        
+def delete_group(base_url, sid, name):
+    url = f"{base_url}/api/groups/{name}"
+    return make_request(url, "DELETE", headers={"sid": sid})
+
+def map_group_ids(prim_groups, sec_groups, list_groups):
+    """Returns a block/allowlist-compatible list of group ids converted for the secondary server's group ids"""
+    mapped_list = []
+    for group_id in list_groups:
+        prim_group = next((d for d in prim_groups if d.get("id") == group_id), None)
+        if prim_group is None:
+            continue
+        sec_group = next((d for d in sec_groups if d.get("name") == prim_group["name"]), None)
+        if sec_group is None:
+            continue
+        mapped_list.append(sec_group["id"])
+    return mapped_list
+        
 
 def update_gravity(base_url, sid):
     """Triggers a gravity db database update by trying known v6 gravity API paths."""
@@ -216,6 +292,20 @@ def update_gravity(base_url, sid):
         print(f"-> Error updating gravity: {e.code}")
         raise
 
+def arrays_equal(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+    
+    # Copy lists so we don't modify the originals
+    l1, l2 = list(arr1), list(arr2)
+    
+    for item in l1:
+        if item in l2:
+            l2.remove(item)  # Removes first occurrence
+        else:
+            return False
+            
+    return len(l2) == 0
 
 def main():
     check_for_pass()
@@ -244,11 +334,36 @@ def main():
         print(f"-> Fetched {len(p_lists)} lists from Primary.")
         print(f"-> Fetched {len(s_lists)} lists from Secondary.")
         
-        p_map = {item["address"].strip().lower(): (item.get("id"), item.get("comment", ""), item.get("enabled")) for item in p_lists}
-        s_map = {item["address"].strip().lower(): (item.get("id"), item.get("comment", ""), item.get("enabled")) for item in s_lists}
+        p_map = {item["address"].strip().lower(): (item.get("id"), item.get("comment", ""), item.get("enabled"), item.get("type"), item.get("groups")) for item in p_lists}
+        s_map = {item["address"].strip().lower(): (item.get("id"), item.get("comment", ""), item.get("enabled"), item.get("type"), item.get("groups")) for item in s_lists}
 
-        print(p_map, s_map)
+        p_groups = get_groups(PRIMARY_PI, p_sid)
+        s_groups = get_groups(SECONDARY_PI, s_sid)
         
+        groups_to_create = []
+        groups_to_delete = []
+        
+        if not arrays_equal(p_groups, s_groups):
+            print("\n\n")
+            missing_groups = [d for d in p_groups if d["name"] not in {item["name"] for item in s_groups}]
+            superfluous_groups = [d for d in s_groups if d["name"] not in {item["name"] for item in p_groups}]
+            for group in missing_groups:
+                groups_to_create.append(group)
+            for group in superfluous_groups:
+                groups_to_delete.append(group)
+                
+        if groups_to_create or groups_to_delete:
+            print("Synchronizing server groups...")
+            for group in groups_to_create:
+                print(f"-> Creating group: {group["name"]}")
+                create_group(SECONDARY_PI, s_sid, group["name"], group["comment"], group["enabled"])
+                print(f"-> Group created: {group["name"]}")
+            for group in groups_to_delete:
+                print(f"-> Deleting group: {group["name"]}")
+                delete_group(SECONDARY_PI, s_sid, group["name"])
+                print(f"-> Group deleted: {group["name"]}")
+        
+
         if LOG_LEVEL == "debug":
             # Debugging: Print addresses to see any hidden mismatch
             print("\n--- DEBUG ADDRESS COMPARISON ---")
@@ -260,53 +375,104 @@ def main():
         to_delete = []
         to_disable = []
         to_enable = []
+        to_allow = []
+        to_block = []
+        to_update_groups = []
         
         # We preserve the original casing from p_lists for the actual addition
         for item in p_lists:
             addr_raw = item["address"]
             addr_key = addr_raw.strip().lower()
+            lists_added_to = 0
             if addr_key not in s_map:
-                to_add.append((addr_raw, item.get("comment", ""), item.get("enabled", True)))
-            elif item.get("enabled") == False and s_map.get(addr_key)[2] == True:
-                to_disable.append((addr_raw, item.get("comment", "")))
+                to_add.append((addr_raw, item.get("comment", ""), item.get("enabled", True), item.get("type"), item.get("groups")))
+                lists_added_to += 1
+            else:  
+                if item.get("enabled") == False and s_map.get(addr_key)[2] == True:
+                    to_disable.append((addr_raw, item.get("comment", ""), item.get("type")))
+                    lists_added_to += 1
+                if item.get("type") == "allow" and s_map.get(addr_key)[3] == "block":
+                    to_allow.append((addr_raw))
+                    lists_added_to += 1
+                if not arrays_equal(item.get("groups"), s_map.get(addr_key)[4]) and lists_added_to < 1:
+                    to_update_groups.append((addr_raw, item.get("type"), item.get("groups")))
                 
         # We preserve the original casing from s_lists for deletion
         for item in s_lists:
             addr_raw = item["address"]
             addr_key = addr_raw.strip().lower()
+            lists_added_to = 0
+
             if addr_key not in p_map:
-                to_delete.append(addr_raw)
-            elif item.get("enabled") == False and p_map.get(addr_key)[2] == True:
-                to_enable.append((addr_raw, item.get("comment", "")))
+                to_delete.append(addr_raw, item.get("type"))
+                lists_added_to += 1
+            else:
+                if item.get("enabled") == False and p_map.get(addr_key)[2] == True:
+                    to_enable.append((addr_raw, item.get("comment", ""), item.get("type")))
+                    lists_added_to += 1
+                if item.get("type") == "allow" and s_map.get(addr_key)[3] == "block":
+                    to_block.append((addr_raw))
+                    lists_added_to += 1
         
         changes_made = False
 
         if to_delete:
             print(f"Removing {len(to_delete)} stale list(s) from Secondary...")
-            for addr in to_delete:
+            for addr, listType in to_delete:
                 print(f"-> Deleting: {addr}")
-                delete_adlist(SECONDARY_PI, s_sid, addr)
+                delete_adlist(SECONDARY_PI, s_sid, addr, listType)
+                print(f"-> Deleted: {addr}")
             changes_made = True
             
         if to_add:
             print(f"Adding {len(to_add)} missing list(s) to Secondary...")
-            for addr, comment, enabled in to_add:
+            for addr, comment, enabled, listType, listGroups in to_add:
                 print(f"-> Adding: {addr}")
-                add_adlist(SECONDARY_PI, s_sid, addr, comment, enabled)
+                add_adlist(SECONDARY_PI, s_sid, addr, comment, enabled, listType, listGroups)
+                print(f"-> Added: {addr}")
             changes_made = True
             
         if to_disable:
             print(f"Disabling {len(to_disable)} lists in Secondary...")
-            for addr, comment in to_disable:
+            for addr, comment, listType in to_disable:
                 print(f"-> Disabling: {addr}")
-                disable_adlist(SECONDARY_PI, s_sid, addr, comment)
+                disable_adlist(SECONDARY_PI, s_sid, addr, comment, listType)
+                print(f"-> Disabled: {addr}")
             changes_made = True
         
         if to_enable:
             print(f"Enabling {len(to_enable)} lists in Secondary...")
-            for addr, comment in to_enable:
+            for addr, comment, listType in to_enable:
                 print(f"-> Enabling: {addr}")
-                enable_adlist(SECONDARY_PI, s_sid, addr, comment)
+                enable_adlist(SECONDARY_PI, s_sid, addr, comment, listType)
+                print(f"-> Enabled {addr}")
+            changes_made = True
+            
+        if to_allow:
+            print(f"Changing {len(to_allow)} lists to allowed in Secondary...")
+            for addr in to_allow:
+                print(f"-> Allowing: {addr}")
+                allow_adlist(SECONDARY_PI, s_sid, addr)
+                print(f"-> Allowed: {addr}")
+            changes_made = True
+            
+        if to_block:
+            print(f"Changing {len(to_block)} lists to blocked in Secondary...")
+            for addr in to_allow:
+                print(f"-> Blocking: {addr}")
+                block_adlist(SECONDARY_PI, s_sid, addr)
+                print(f"-> Blocked: {addr}")
+            changes_made = True
+            
+        if to_update_groups:
+            print(f"Updating groups for {len(to_update_groups)} lists in Secondary...")
+            for addr, listType, groups in to_update_groups:
+                print(f"Groups: {groups}")
+                mapped_groups = map_group_ids(p_groups, s_groups, groups)
+                print(f"Mapped Groups: {mapped_groups}")
+                print(f"-> Updating list groups: {addr}")
+                update_adlist_groups(SECONDARY_PI, s_sid, addr, listType, mapped_groups, p_groups, s_groups)
+                print(f"-> List groups Updated")
             changes_made = True
             
         if changes_made:
